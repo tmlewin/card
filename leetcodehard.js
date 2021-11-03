2bfea907a8ad13566d42b5f1aa41d65ee3ed00df
tmlewin <dexterity4u@gmail.com>
2021-11-02 20:38:25 -0500

    "update"

--- leetcodehard.js
+++ leetcodehard.js
@@ -38,25 +38,25 @@
 // p contains only lowercase English letters, '.', and '*'.
 // It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.
 
-var isMatch =
-    function(s, p) {
-        let dp = Array(s.length + 1).fill(null).map(() => Array(p.length + 1).fill(false));
-        dp[0][0] = true;
-        for (let i = 1; i <= p.length; i++) {
-            if (p[i - 1] === '*') dp[0][i] = dp[0][i - 2];
-        }
-        for (let i = 1; i <= s.length; i++) {
-            for (let j = 1; j <= p.length; j++) {
-                if (p[j - 1] === '.' || p[j - 1] === s[i - 1]) dp[i][j] = dp[i - 1][j - 1];
-                else if (p[j - 1] === '*') {
-                    if (p[j - 2] === '.' || p[j - 2] === s[i - 1]) dp[i][j] = dp[i - 1][j] || dp[i][j - 2];
-                    else dp[i][j] = dp[i][j - 2];
-                }
-            }
-        }
-        return dp[s.length][p.length];
+// var isMatch =
+//     function(s, p) {
+//         let dp = Array(s.length + 1).fill(null).map(() => Array(p.length + 1).fill(false));
+//         dp[0][0] = true;
+//         for (let i = 1; i <= p.length; i++) {
+//             if (p[i - 1] === '*') dp[0][i] = dp[0][i - 2];
+//         }
+//         for (let i = 1; i <= s.length; i++) {
+//             for (let j = 1; j <= p.length; j++) {
+//                 if (p[j - 1] === '.' || p[j - 1] === s[i - 1]) dp[i][j] = dp[i - 1][j - 1];
+//                 else if (p[j - 1] === '*') {
+//                     if (p[j - 2] === '.' || p[j - 2] === s[i - 1]) dp[i][j] = dp[i - 1][j] || dp[i][j - 2];
+//                     else dp[i][j] = dp[i][j - 2];
+//                 }
+//             }
+//         }
+//         return dp[s.length][p.length];
 
-}
+// }
 
 
 
@@ -65,4 +65,50 @@ var isMatch =
 
 
     
-console.log(isMatch('aa', 'a'))
+// console.log(isMatch('aa', 'a'))
+
+
+
+
+
+// function plusMinus(arr,num) {
+//     let pos = 0;
+//     let neg = 0;
+//     let zero = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] > 0) pos++;
+//         else if (arr[i] < 0) neg++;
+//         else zero++;
+//     }
+//     console.log((pos / num).toFixed(6));
+//     console.log((neg / num).toFixed(6));
+//     console.log((zero / num).toFixed(6));
+ 
+// }
+
+// plusMinus([-4, 3, -9, 0, 4, 1],6)
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
