038a5e0b10a36d6831250cab866a9f628955fb95
tmlewin <dexterity4u@gmail.com>
2021-11-07 19:10:06 -0600

    "update"

--- leetcodehard.js
+++ leetcodehard.js
@@ -58,6 +58,13 @@
 
 // }
 
+ 
+
+
+
+
+
+
 
 
 
@@ -89,7 +96,217 @@
 // plusMinus([-4, 3, -9, 0, 4, 1],6)
 
 
+// create a function for anagrams that takes in two strings and returns true if they are anagrams and false if they are not
 
+
+// create a merge sort function that takes in an array and returns a sorted array
+
+// fizzbuzz
+
+
+//  function fizzBuzz(n) {
+//     let arr = [];
+//     for (let i = 1; i <= n; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) arr.push('FizzBuzz');
+//         else if (i % 3 === 0) arr.push('Fizz');
+//         else if (i % 5 === 0) arr.push('Buzz');
+//         else arr.push(i);
+//     }
+//     return arr;
+
+//     }
+
+
+// console.log(fizzBuzz(15))
+
+// create a random forest classification model function that takes in an array of objects and returns a classification model that can be used to predict the class of an object
+
+
+//  function randomForestClassifier(arr) {
+//     let classifier = {};
+//     let classArr = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         if (!classArr.includes(arr[i].class)) classArr.push(arr[i].class);
+//     }
+//     for (let i = 0; i < classArr.length; i++) {
+//         classifier[classArr[i]] = {};
+//         for (let j = 0; j < arr.length; j++) {
+//             if (!classifier[classArr[i]][arr[j].feature]) classifier[classArr[i]][arr[j].feature] = 0;
+//             classifier[classArr[i]][arr[j].feature] += 1;
+//         }
+//     }
+//     return classifier;
+// }
+
+
+//  console.log(randomForestClassifier([{      // {'class': 'A', 'feature': 'a'}
+//     'class': 'A',
+//     'feature': 'a'
+// }, {
+
+//     'class': 'A',
+//     'feature': 'b'
+// }, {
+//     'class': 'A',
+//     'feature': 'c'
+// }, {
+//     'class': 'B',
+//     'feature': 'a'
+// }, {
+//     'class': 'B',
+//     'feature': 'b'
+// }, {
+//     'class': 'B',
+//     'feature': 'c'
+// }, {
+//     'class': 'C',
+//     'feature': 'a'
+// }, {
+//     'class': 'C',
+//     'feature': 'b'
+// }, {
+//     'class': 'C',
+//     'feature': 'c'
+// }]))
+
+
+// create a fizzbuzz function   that takes in a number and returns a string that is the fizzbuzz of that number 
+
+
+//  function fizzBuzz(n) {
+//     let arr = [];
+//     for (let i = 1; i <= n; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) arr.push('FizzBuzz');
+//         else if (i % 3 === 0) arr.push('Fizz');
+//         else if (i % 5 === 0) arr.push('Buzz');
+//         else arr.push(i);
+//     }
+//     return arr;
+
+
+//     }
+
+
+//     console.log(fizzBuzz(15))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
 
 
 
