b30de80323dd951f871848e7b5e49a0e3e8fa391
tmlewin <dexterity4u@gmail.com>
2022-03-30 18:36:04 -0500

    "updates"

---
+++ prcss.js
@@ -0,0 +1,44 @@
+// Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+// Integers in each row are sorted from left to right.
+// The first integer of each row is greater than the last integer of the previous row.
+ 
+
+// Example 1:
+
+
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+// Output: true
+// Example 2:
+
+
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+// Output: false
+ 
+
+// Constraints:
+
+// m == matrix.length
+// n == matrix[i].length
+// 1 <= m, n <= 100
+// -104 <= matrix[i][j], target <= 104
+
+
+// Solution 1
+// Time O(m+n)
+// Space O(1)
+
+
+var searchMatrix = function(matrix, target) {
+    if(matrix.length === 0) return false;
+    let row = 0;
+    let col = matrix[0].length - 1;
+    while(row < matrix.length && col >= 0) {
+        if(matrix[row][col] === target) return true;
+        else if(matrix[row][col] > target) col--;
+        else row++;
+    }
+    return false;
+};
+
+
