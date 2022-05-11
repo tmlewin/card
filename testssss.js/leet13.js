14a66c4eebdff35a9246d45de8d654c9dbae49a3
tmlewin <dexterity4u@gmail.com>
2022-05-10 19:57:27 -0500

    "update"

--- testssss.js/leet13.js
+++ testssss.js/leet13.js
@@ -1,8 +1,56 @@
-// You are given an integer array nums and an integer k.
+// Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
 
-// In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
+// You have the following three operations permitted on a word:
 
-// Return the maximum number of operations you can perform on the array.
-
+// Insert a character
+// Delete a character
+// Replace a character
  
+
+// Example 1:
+
+// Input: word1 = "horse", word2 = "ros"
+// Output: 3
+// Explanation: 
+// horse -> rorse (replace 'h' with 'r')
+// rorse -> rose (remove 'r')
+// rose -> ros (remove 'e')
+// Example 2:
+
+// Input: word1 = "intention", word2 = "execution"
+// Output: 5
+// Explanation: 
+// intention -> inention (remove 't')
+// inention -> enention (replace 'i' with 'e')
+// enention -> exention (replace 'n' with 'x')
+// exention -> exection (replace 'n' with 'c')
+// exection -> execution (insert 'u')
+ 
+
+// Constraints:
+
+// 0 <= word1.length, word2.length <= 500
+// word1 and word2 consist of lowercase English letters.
+
+
+var minDistance = function(word1, word2) {
+    let dp = new Array(word1.length + 1).fill(0).map(() => new Array(word2.length + 1).fill(0));
+    for (let i = 0; i <= word1.length; i++) {
+        dp[i][0] = i;
+    }
+    for (let j = 0; j <= word2.length; j++) {
+        dp[0][j] = j;
+    }
+    for (let i = 1; i <= word1.length; i++) {
+        for (let j = 1; j <= word2.length; j++) {
+            dp[i][j] = Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + (word1[i - 1] === word2[j - 1] ? 0 : 1));
+        }
+    }
+    return dp[word1.length][word2.length];
+}
+
+
+console.log(minDistance("horse", "ros"));
+
+
 
